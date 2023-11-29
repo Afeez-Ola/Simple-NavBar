6670e87531a96496cac3a81375e4a76cf381fda7
Afeez Bolaji <afeezbolajiola@gmail.com>
2023-11-29 21:22:48 +0100

    .

--- index.js
+++ index.js
@@ -1,10 +1,6 @@
 const mobileButton = document.querySelector('.mobile_button');
 
 mobileButton.addEventListener('click', () => {
- //  const primary_nav = document.querySelector('.primary_nav');
- //  const secondary_nav = document.querySelector('.secondary_nav');
- //  primary_nav.classList.toggle('hidden');
- //  secondary_nav.classList.toggle('hidden');
  const mobileNav = document.querySelector('.mobile_nav');
  mobileNav.classList.toggle('hidden');
 });
